13405e5e6c0879f5ec670166c88dc381833598fa
Adrien-Ray <a.raymond@codeur.online>
2023-09-16 17:30:37 +0200

    setup strapi

---
+++ src/api/caracters-type/routes/caracters-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * caracters-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::caracters-type.caracters-type');
---
+++ src/api/caracters-type/services/caracters-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * caracters-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::caracters-type.caracters-type');
